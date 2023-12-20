7fcd9980dc2c3952afbdd7ee3fdabcf80cb3e1f6
Shivam Gupta <codequillcrafts@gmail.com>
2023-12-20 07:19:52 +0530

    adding files

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ let quote = options.querySelectorAll('span')[3];
 let custom = options.querySelectorAll('span')[4];
 
 let typingContainer = document.querySelector('.typingContainer');
-let reader = typingContainer.querySelectorAll('input')[0];
-let writer = typingContainer.querySelectorAll('input')[1];
+let reader = typingContainer.querySelectorAll('p')[0];
+let writer = typingContainer.querySelectorAll('p')[1];
 
 let results = document.querySelector('.results');
 let wpm = results.querySelectorAll('input')[0];
@@ -17,4 +17,16 @@ let accuracy = results.querySelectorAll('input')[1];
 let mistakes = results.querySelectorAll('input')[2];
 let timeLeft = results.querySelectorAll('input')[3];
 
-reader.value = textModule.randomText();
+reader.innerHTML = textModule.randomText();
+
+document.addEventListener('DOMContentLoaded', function () {
+    writer.focus();
+})
+
+// writer.addEventListener('scroll', function () {
+//     // Check if the writer input is at the end
+//     if (writer.scrollLeft + writer.clientWidth >= writer.scrollWidth) {
+//         // Synchronize the reader input's scroll position
+//         reader.scrollLeft = writer.scrollLeft;
+//     }
+// });
